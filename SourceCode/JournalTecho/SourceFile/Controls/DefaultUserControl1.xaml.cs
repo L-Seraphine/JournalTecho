099ed67947993cf0112a0dda669abc4ef0551a30
LSeraphine <738232585@qq.com>
2023-02-02 21:59:54 +0800

    创建基本UI框架,创建了基本用户控件

---
+++ SourceCode/JournalTecho/SourceFile/Controls/DefaultUserControl1.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace JournalTecho.SourceFile.Controls
+{
+    /// <summary>
+    /// DefaultUserControl1.xaml 的交互逻辑
+    /// </summary>
+    public partial class DefaultUserControl1 : UserControl
+    {
+        //【注册自定义属性】
+        //标题属性（默认值：标题）
+        public static DependencyProperty TitlesProperty =
+            DependencyProperty.Register("Titles", typeof(string), typeof(DefaultUserControl1), new PropertyMetadata("标题"));
+        //标题字体大小属性（默认值：12）
+        public static DependencyProperty TitleFontSizeProperty =
+            DependencyProperty.Register("TitleFontSize", typeof(string), typeof(DefaultUserControl1), new PropertyMetadata("12"));
+        //内容属性（默认值：内容）
+        public static DependencyProperty TextsProperty =
+            DependencyProperty.Register("Texts", typeof(string), typeof(DefaultUserControl1), new PropertyMetadata("内容"));
+
+        //【定义自定义属性】
+        [BindableAttribute(true)]
+        public string Titles
+        {
+            get { return (string)GetValue(TitlesProperty); }
+            set { SetValue(TitlesProperty, value); }
+        }
+        public string Texts
+        {
+            get { return (string)GetValue(TextsProperty); }
+            set { SetValue(TextsProperty, value); }
+        }
+        public string TitleFontSize
+        {
+            get { return (string)GetValue(TitleFontSizeProperty); }
+            set { SetValue(TitleFontSizeProperty, value); }
+        }
+
+        //【构造函数】
+        public DefaultUserControl1()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
---
+++ SourceCode/JournalTecho/SourceFile/Controls/DefaultUserControl2.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace JournalTecho.SourceFile.Controls
+{
+    /// <summary>
+    /// DefaultUserControl2.xaml 的交互逻辑
+    /// </summary>
+    public partial class DefaultUserControl2 : UserControl
+    {
+        public DefaultUserControl2()
+        {
+            InitializeComponent();
+        }
+    }
+}
