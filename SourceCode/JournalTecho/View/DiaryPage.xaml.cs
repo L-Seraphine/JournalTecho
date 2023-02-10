9c80290f484ebbf5cfa4425d8ecff63e0e1e87d7
LSeraphine <738232585@qq.com>
2023-02-11 00:23:15 +0800

    添加了一些空页面

---
+++ SourceCode/JournalTecho/View/DiaryPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JournalTecho.View
+{
+    /// <summary>
+    /// DiaryPage.xaml 的交互逻辑
+    /// </summary>
+    public partial class DiaryPage : Page
+    {
+        private static DiaryPage instance = null;
+        private DiaryPage()
+        {
+            InitializeComponent();
+        }
+        public static DiaryPage GetDiaryPage()
+        {
+            if (instance == null)
+            {
+                instance = new DiaryPage();
+            }
+            return instance;
+        }
+    }
+}
--- SourceCode/JournalTecho/ViewModel/MainWindowViewModel.cs
+++ SourceCode/JournalTecho/ViewModel/MainWindowViewModel.cs
@@ -12,10 +12,16 @@ namespace JournalTecho.ViewModel
     {
         //打开设置窗口
         public ICommand OpenSetting { get; set; }
-        //打开日历页面
-        public ICommand Calendar { get; set; }
         //打开首页页面
         public ICommand Main { get; set; }
+        //打开日历页面
+        public ICommand Calendar { get; set; }
+        //打开日记页面
+        public ICommand Diary { get; set; }
+        //打开点子页面
+        public ICommand Idea { get; set; }
+        //打开时间轴页面
+        public ICommand TimeLine { get; set; }
 
         //构造函数
         public MainWindowViewModel(Frame mainframe)
@@ -26,17 +32,34 @@ namespace JournalTecho.ViewModel
                 settings.Show();    //显示设置页面
             });
 
+            //打开首页
+            Main = new Command((arg) => {
+                MainPage mainPage = MainPage.GetMainPage();     //获取首页
+                mainframe.Navigate(mainPage);   //导航到首页
+            });
+
             //打开日历
             Calendar = new Command((arg)=> {
                 CalendarPage calendarPage = CalendarPage.GetCalendarPage();     //获取日历页面
                 mainframe.Navigate(calendarPage);   //导航到日历页面
             });
 
-            //打开首页
-            Main = new Command((arg) => {
-                MainPage mainPage = MainPage.GetMainPage();     //获取首页
-                mainframe.Navigate(mainPage);   //导航到首页
+            //打开日记
+            Diary = new Command((arg)=> {
+                DiaryPage diaryPage = DiaryPage.GetDiaryPage();     //获取日历页面
+                mainframe.Navigate(diaryPage);   //导航到日历页面
             });
+
+            //打开点子
+            Idea = new Command((arg) => {
+                MessageBox.Show("开发中");
+            });
+
+            //打开时间轴
+            TimeLine = new Command((arg) => {
+                MessageBox.Show("开发中");
+            });
+
         }
         
     }
